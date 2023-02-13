8df264f61879446863f94a588860b889f72c09af
Ilham808 <budiawanilham04@gmail.com>
2023-02-13 10:50:26 +0700

    Rename folder backend-copy to siswa

--- backend/views/layouts/menu.php
+++ backend/views/layouts/menu.php
@@ -20,7 +20,7 @@ echo Menu::widget(
             ],
             [
                 'template' => '<a class="sidebar-link waves-effect waves-dark sidebar-link" href="/backend/tahun-ajaran/index" aria-expanded="false">
-                <i class="mdi mdi-account-network"></i>
+                <i class="mdi mdi-calendar"></i>
                 <span class="hide-menu">Tahun Ajaran</span>
                 </a>',
                 'options' => [
@@ -40,7 +40,7 @@ echo Menu::widget(
             ],
             [
                 'template' => '<a class="sidebar-link waves-effect waves-dark sidebar-link" href="/backend/mata-pelajaran/index" aria-expanded="false">
-                <i class="mdi mdi-account-network"></i>
+                <i class="mdi mdi-book-multiple"></i>
                 <span class="hide-menu">Mata Pelajaran</span>
                 </a>',
                 'options' => [
@@ -50,7 +50,7 @@ echo Menu::widget(
             ],
             [
                 'template' => '<a class="sidebar-link waves-effect waves-dark sidebar-link" href="/backend/kelas/index" aria-expanded="false">
-                <i class="mdi mdi-account-network"></i>
+                <i class="mdi mdi-domain"></i>
                 <span class="hide-menu">Kelas</span>
                 </a>',
                 'options' => [
@@ -60,8 +60,18 @@ echo Menu::widget(
             ],
             [
                 'template' => '<a class="sidebar-link waves-effect waves-dark sidebar-link" href="/backend/guru/index" aria-expanded="false">
-                <i class="mdi mdi-account-network"></i>
+                <i class="mdi mdi-account-multiple"></i>
                 <span class="hide-menu">Guru</span>
+                </a>',
+                'options' => [
+                    'class' => 'sidebar-item'
+                ],
+                'active' => Yii::$app->request->url === Yii::$app->homeUrl
+            ],
+            [
+                'template' => '<a class="sidebar-link waves-effect waves-dark sidebar-link" href="/backend/siswa/index" aria-expanded="false">
+                <i class="mdi mdi-account-multiple"></i>
+                <span class="hide-menu">Siswa</span>
                 </a>',
                 'options' => [
                     'class' => 'sidebar-item'
